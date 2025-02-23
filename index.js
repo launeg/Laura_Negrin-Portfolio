e64ded4cd7078221bd3d6c990950e94b9d12a79f
Laura Negrin <negrinagarcialaura@gmail.com>
2025-02-23 16:42:57 -0500

    Game added to index.html

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.getElementById('gameCanvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
-const gravity = 0.5
+// Make the canvas full screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-class Sprite {
-    constructor({position, imageSrc}) {
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
-
-    draw() {
-        if (!this.image) return
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-    update() {
-        this.draw()
-    }
-}
-
+const gravity = 0.5;
 
 class Player {
   constructor(position) {
@@ -29,32 +14,33 @@ class Player {
       x: 0,
       y: 1
     }
-    this.height = 100
+    this.height = 150
+    this.width = 150
   }
 
-  draw() {
-    c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 100, this.height)
+//   draw() {
+//     c.fillStyle = 'red'
+//     c.fillRect(this.position.x, this.position.y, this.width, this.height)
+//   }
+draw() {
+    const img = new Image();
+    img.src = 'images/tut.png';
+    c.drawImage(img, this.position.x, this.position.y, this.width, this.height);
   }
-    update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else { this.velocity.y = 0 }
-        }
-        
-    }
-
+  
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+    if (this.position.y + this.height + this.velocity.y < canvas.height)
+        this.velocity.y += gravity
+    else { this.velocity.y = 0 }
+  }
+}
 
 const player = new Player({
-        x: 100,
-        y: 100
-    })
-const player2 = new Player({
-    x: 300,
-    y: 100
+    x: 50,
+    y: 50
 })
 
 const keys = {
@@ -63,34 +49,14 @@ const keys = {
     w: {pressed: false},
 }
 
-const background = new Sprite({
-    position: {
-    x: 0,
-    y: 0, 
-    },
-    imageSrc: './images/NY.png',
-})
-
-
-
-
 function animate() {
   window.requestAnimationFrame(animate)
-  c.fillStyle = 'white'
-  c.fillRect(0,0, canvas.width, canvas.height)
-  
-  c.save()
-  c.scale(.57,.579)
-  background.update()
-  c.restore()
+  c.clearRect(0, 0, canvas.width, canvas.height);
   player.update()
-  player2.update()
-
   player.velocity.x = 0
-  if (keys.d.pressed) player.velocity.x = 4
-  else if (keys.a.pressed) player.velocity.x = -4
+  if (keys.d.pressed && player.position.x < canvas.width - player.width) player.velocity.x = 6
+  else if (keys.a.pressed && player.position.x > 0) player.velocity.x = -6
 }
-
 
 animate()
 
@@ -103,21 +69,25 @@ window.addEventListener('keydown', (event) => {
             keys.a.pressed = true
             break
         case 'w':
-            player.velocity.y = -19
-
+            if (player.position.y === canvas.height - player.height)
+                player.velocity.y = -20
             break    
     }
+})
 
-    })
+window.addEventListener('keyup', (event) => {
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break 
+    }
+})
 
-    window.addEventListener('keyup', (event) => {
-        switch(event.key) {
-            case 'd':
-                keys.d.pressed = false
-                break
-            case 'a':
-                keys.a.pressed = false
-                break 
-        }
-    
-        })
+// Update canvas size on window resize
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
